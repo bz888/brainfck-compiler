af7d22f6a6eb79795f485c02a10ef433d1b0be22
bz888 <ziyu.ben158@gmail.com>
2022-02-02 05:02:35 +1300

    setup

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Form () {
+  function handleChange (e) {
+
+  }
   return (
     <div>
       <textarea></textarea>
+      <button onChange={handleChange}/>
       <p>test</p>
 
     </div>

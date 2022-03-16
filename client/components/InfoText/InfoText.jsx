e2ac9af0524c1fde0374d525e5bbe85974ad27da
bz888 <ziyu.ben158@gmail.com>
2022-03-17 09:44:25 +1300

    change info text

--- client/components/InfoText/InfoText.jsx
+++ client/components/InfoText/InfoText.jsx
@@ -3,7 +3,7 @@ export default function InfoText () {
   return (
     <div className='info-div'>
       <div>
-        <span>Please be nice, this is a really simple interpreter and is not optimized for juice brainfuck</span>
+        <span>Please be nice, this is a really simple interpreter and is not optimized for extreme brainfuck juices</span>
       </div>
     </div>
   )
